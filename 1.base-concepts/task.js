b8548c25fadf8c2c2a389396fd289647bced5773
barmenkovdmitrii <bds@porter.pro>
2022-08-17 14:09:47 +0300

    PC

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,14 @@
 function solveEquation(a, b, c) {
-  // тестируем загрузку
   let arr = [];
   let discriminant = Math.pow(b, 2) - 4 * a * c;
 
-  if ( discriminant == 0) {
+  if ( discriminant == 0){
     arr = [-b / 2 * a];
   }
 
   if (discriminant > 0){
     arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
   }
-
 
   return arr; // array
 }
@@ -22,3 +20,4 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   return totalAmount;
 }
+
