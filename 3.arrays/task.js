08c66d37c3e0187fcc2efcae14e2e3c44157a628
barmenkovdmitrii <bds@porter.pro>
2022-08-22 14:39:15 +0300

    mac

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 
   // Ваш код
 
-  return result = arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]); // boolean
+  return result; // boolean
 }
 
 function advancedFilter(arr) {
