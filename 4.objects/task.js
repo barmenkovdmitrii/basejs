9d415f9e951c8805518cdec69f60d2ce587bd533
barmenkovdmitrii <bds@porter.pro>
2022-08-23 16:43:12 +0300

    mac

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,16 +1,17 @@
 function Student(name, gender, age) {
     // Ваш код
-   let userStudent = {
-       name,
-       gender,
-       age,
-   }
+   this.name = name;
+   this.gender = gender;
+   this.age = age;
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
+    this.subjectName = subjectName;
 }
- let student1 = new Student("oleg", "men", 22);
- let student2 = new Student("Vasya","man",32);
+ let userStudent1 = new Student("oleg", "man", 22);
+ let userStudent2 = new Student("Vasya","man",32);
+
+ userStudent2.subjectName = "English";
 // ваш код для остальных методов
