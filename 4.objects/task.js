6c56b35e325e1eee34ef69d2b6d927e4f42db06c
barmenkovdmitrii <bds@porter.pro>
2022-08-23 17:23:51 +0300

    mac

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -10,8 +10,23 @@ Student.prototype.setSubject = function (subjectName) {
   //ваш код
     this.subjectName = subjectName;
 }
+
+Student.prototype.addMark = function (mark) {
+    this.addMark = mark;
+
+}
+
  let userStudent1 = new Student("oleg", "man", 22);
  let userStudent2 = new Student("Vasya","man",32);
 
- userStudent2.subjectName = "English";
+ userStudent2.setSubject("English");
+ userStudent2.addMark = 5;
+
+
+if(this.marks === undefined){
+    // добавить первую оценку
+} else {
+    // добавить вторую и последующие оценки в массив
+}
+
 // ваш код для остальных методов
