436c111d9b00b8d3a3e7a3e466fa0dd7f748bd8e
barmenkovdmitrii <bds@porter.pro>
2022-08-28 20:10:20 +0300

    PC

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,3 +1,15 @@
-class Task {
-    
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+
+    }
+
+}
+
+PrintEditionItem.prototype.fix = function (){
+    return this.state * 1.5;
 }
