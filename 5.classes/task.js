61355ba65e8080db307890bcf77bdb68c548a6ba
barmenkovdmitrii <bds@porter.pro>
2022-09-06 15:11:52 +0300

    mac

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -69,6 +69,61 @@ class DetectiveBook extends Book {
     }
 }
 
+class DetectiveBook extends Book{
+    constructor(...args) {
+        super(...args);
+        this.type = "detective";
+    }
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book){
+        if (book.state > 30){
+            this.books[length] = book;
+        }
+    }
+
+    findBookBy(type, value){
+        return (this.books.find(book => book[type]  === value) ?? null);
+    }
+
+    giveBookByName(bookName){
+        return this.books.splice(this.books.indexOf(this.findBookBy("name", bookName)), 1)[0] ?? null;
+    }
+}
+
+class Student {
+    constructor(name) {
+        this.name = name;
+        this.subjects = [];
+    }
+
+    addMark(mark, subject){
+        if (this.subjects.find(obj => obj.name === subject) === undefined){
+            this.subjects.push({name: subject, marks: []});
+        }
+        this.subjects.find(obj => obj.name === subject).marks.push(mark);
+    }
+
+    getAverageBySubject(subject){
+        let marksArr = this.subjects.find(obj => obj.name === subject).marks;
+        return marksArr.reduce(function (sum, element) {return sum+element;}, 0) / marksArr.length;
+    }
+
+    getAverage(){
+        let marksArr = [];
+        this.subjects.forEach(function (elem) {
+            marksArr.push(elem.marks.reduce(function (sum, element) {return sum+element;}, 0) / elem.marks.length);
+        });
+        return marksArr.reduce(function (sum, element) {return sum+element;}, 0) / marksArr.length;
+    }
+}
+
 const sherlock = new PrintEditionItem(
     "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
     2019,
