4636ea4032f85b2f0e20db5f934c2a048b909632
barmenkovdmitrii <bds@porter.pro>
2022-09-07 12:53:20 +0300

    mac

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,3 +1,13 @@
+let parseCount = () => {
+    if(isNaN(Number.parseInt())){
+        throw new SyntaxError("Невалидное значение");
+    }else{
+        return Number.parseInt();
+    }
+
+}
+
+
 class Triangle {
     constructor(a, b,c) {
 
